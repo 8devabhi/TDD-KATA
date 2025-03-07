5842247bf039b9cd2ddd5fae5640ef28cea8cc4e
8devabhi <abhinava.dev@gmail.com>
2025-03-07 17:34:10 +0530

    Refactoring for maintaining the already passed test case which broke while passing new one

--- src/utils/StringCalculator.ts
+++ src/utils/StringCalculator.ts
@@ -2,6 +2,9 @@ export class StringCalculator {
   add(numeralString: string): number {
     if (!numeralString) return 0;
     const splittedNumberList = numeralString.split(",").map(Number);
+    if (splittedNumberList?.length === 1) {
+      return splittedNumberList[0]
+    }
     return (splittedNumberList[0] + splittedNumberList[1]);
   }
 }
