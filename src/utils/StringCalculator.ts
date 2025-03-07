df162047c733b077a6543c1476af4fa8181eeaea
8devabhi <abhinava.dev@gmail.com>
2025-03-07 19:51:43 +0530

    Refactor

--- src/utils/StringCalculator.ts
+++ src/utils/StringCalculator.ts
@@ -1,6 +1,16 @@
 export class StringCalculator {
+
+  private static readonly DELIMITER_REGEX = /,|\n/;
+
   add(numeralString: string): number {
     if (!numeralString) return 0;
-    return numeralString.split(/,|\n/).map(Number).reduce((sum, num) => sum + num, 0);
+    const numberList = this.convertNumeralStringToArray(numeralString);
+    return this.calculateSum(numberList);
+  }
+  private convertNumeralStringToArray(numbers: string): number[] {
+    return numbers.split(StringCalculator.DELIMITER_REGEX).map(Number);
+  }
+  private calculateSum(numbers: number[]): number {
+    return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
