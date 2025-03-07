8994b50dedcf31eb1fd0f7512ac4f135fa5ad08f
8devabhi <abhinava.dev@gmail.com>
2025-03-07 20:35:36 +0530

    Code Refactored

--- src/utils/StringCalculator.ts
+++ src/utils/StringCalculator.ts
@@ -1,22 +1,27 @@
 export class StringCalculator {
 
-  private static readonly DELIMITER_REGEX = /,|\n/;
+  private static readonly DEFAULT_DELIMITER_REGEX = /,|\n/;
+  private static readonly CUSTOM_DELIMITER_PATTERN = /^\/\/(.+)\n/;
 
-  add(numeralString: string): number {
-    if (!numeralString) return 0;
-    const numberList = this.convertNumeralStringToArray(numeralString);
+  add(inputString: string): number {
+    if (!inputString) return 0;
+    const { delimiter, numberString } = this.extractDelimiter(inputString);
+    const numberList = this.convertNumeralStringToArray(numberString, delimiter);
     return this.calculateSum(numberList);
   }
-  private convertNumeralStringToArray(numeralString: string): number[] {
-    let delimiterRegex = StringCalculator.DELIMITER_REGEX;
-    if (numeralString.startsWith("//")) {
-      const match = numeralString.match(/^\/\/(.+)\n/);
-      if (match) {
-        delimiterRegex = new RegExp(`[${match[1]}]`);
-        numeralString = numeralString.replace(/^\/\/.+\n/, "");
-      }
+  private convertNumeralStringToArray(numeralString: string, delimiter: RegExp): number[] {
+    return numeralString.split(delimiter).map(Number);
+  }
+  private extractDelimiter(numeralString: string): { delimiter: RegExp; numberString: string } {
+    const match = numeralString.match(StringCalculator.CUSTOM_DELIMITER_PATTERN);
+
+    if (match) {
+      const customDelimiter = match[1];
+      const numberString = numeralString.replace(StringCalculator.CUSTOM_DELIMITER_PATTERN, "");
+      return { delimiter: new RegExp(customDelimiter), numberString };
     }
-    return numeralString.split(delimiterRegex).map(Number);
+
+    return { delimiter: StringCalculator.DEFAULT_DELIMITER_REGEX, numberString: numeralString };
   }
   private calculateSum(numbers: number[]): number {
     return numbers.reduce((sum, num) => sum + num, 0);
