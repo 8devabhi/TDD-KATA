f11d6fabf1691c54223e4f052e2cc9acec1e1c3e
8devabhi <abhinava.dev@gmail.com>
2025-03-07 20:16:11 +0530

    Code for supporting test case: custom delimiters

--- src/utils/StringCalculator.ts
+++ src/utils/StringCalculator.ts
@@ -7,8 +7,16 @@ export class StringCalculator {
     const numberList = this.convertNumeralStringToArray(numeralString);
     return this.calculateSum(numberList);
   }
-  private convertNumeralStringToArray(numbers: string): number[] {
-    return numbers.split(StringCalculator.DELIMITER_REGEX).map(Number);
+  private convertNumeralStringToArray(numeralString: string): number[] {
+    let delimiterRegex = StringCalculator.DELIMITER_REGEX;
+    if (numeralString.startsWith("//")) {
+      const match = numeralString.match(/^\/\/(.+)\n/);
+      if (match) {
+        delimiterRegex = new RegExp(`[${match[1]}]`);
+        numeralString = numeralString.replace(/^\/\/.+\n/, "");
+      }
+    }
+    return numeralString.split(delimiterRegex).map(Number);
   }
   private calculateSum(numbers: number[]): number {
     return numbers.reduce((sum, num) => sum + num, 0);
