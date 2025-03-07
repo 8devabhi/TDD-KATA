6265e83699992cf47c680fdddd161a9e8a472036
8devabhi <abhinava.dev@gmail.com>
2025-03-07 17:20:19 +0530

    code for passing test case of sum of 2 numbers

--- src/utils/StringCalculator.ts
+++ src/utils/StringCalculator.ts
@@ -1,6 +1,7 @@
 export class StringCalculator {
-  add(numbers: string): number {
-    if (!numbers) return 0;
-    return parseInt(numbers);
+  add(numeralString: string): number {
+    if (!numeralString) return 0;
+    const splittedNumberList = numeralString.split(",").map(Number);
+    return (splittedNumberList[0] + splittedNumberList[1]);
   }
 }
